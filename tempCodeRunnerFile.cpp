4a490381f8577d916c63e0bf6205d26758fd4a91
iAmSomeBoy <ashikur2401@gmail.com>
2025-11-03 21:54:59 +0600

    ...

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for(auto &[rolls, names, depts, cgs]: Student){ //c++ 17 and uper vesion
+    //     cout<<rolls<<" "<<names<<" "<<depts<<" "<<cgs<<endl;
+    // }
--- tuple.cpp
+++ tuple.cpp
@@ -1,31 +1,59 @@
-//tupe is fore more than 2 type of data types at a time
-//tuple<int, int, int, int, .........>
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    cout << "Input Roll, Name, Department, CGPA\n";
 
-    cout<<"Input Roll, Name, Department, CGPA\n";    
     int roll;
     string name, dept;
     double cg;
 
-    cin>>roll;
-    cin.ignore();
+    cin >> roll;
+    cin.ignore(); // clear newline
     getline(cin, name);
-    cin.ignore();
     getline(cin, dept);
-    cin>>cg;
+    cin >> cg;
 
     tuple<int, string, string, double> Student(roll, name, dept, cg);
-    //student = make_tuple(roll, name, dept, cg);
 
-    cout<<"Student Data\n";
-    cout<<get<0>(Student)<<"\t"<<get<1>(Student) <<"\t"<<get<2>(Student)<<"\t"<<get<3>(Student)<<endl;
+    cout << "\nSingle Student Data:\n";
+    cout << get<0>(Student) << "\t" 
+         << get<1>(Student) << "\t" 
+         << get<2>(Student) << "\t" 
+         << get<3>(Student) << endl;
 
+    // -----------------------------------------
+    cout << "\nFor more students:\n";
+    cout << "Enter number of students: ";
+    int N;
+    cin >> N;
+    cin.ignore(); // clear newline
 
+    vector<tuple<int, string, string, double>> Students;
 
+    for (int i = 0; i < N; i++) {
+        int rolls;
+        string names, depts;
+        double cgs;
 
+        cout << "\nStudent " << i + 1 << " (Roll, Name, Department, CGPA):\n";
+        cin >> rolls;
+        cin.ignore();  // clear newline before getline
+        getline(cin, names);
+        getline(cin, depts);
+        cin >> cgs;
+        cin.ignore();  // clear newline for next iteration
+
+        Students.push_back(make_tuple(rolls, names, depts, cgs));
+    }
+
+    cout << "\nAll Student Data:\n";
+    for (int i = 0; i < Students.size(); i++) {
+        cout << get<0>(Students[i]) << "\t"
+             << get<1>(Students[i]) << "\t"
+             << get<2>(Students[i]) << "\t"
+             << get<3>(Students[i]) << endl;
+    }
 
     return 0;
 }
